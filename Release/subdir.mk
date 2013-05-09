5de101786ea3957ebc0765302ecd2c551f0c34a3
Carl Sandaker <carl@gunnar-linux-pc.(none)>
2013-05-09 12:03:43 +0200

    adding own nsif code for the first time

---
+++ Release/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../new-stuff.c \
+../new-test.c 
+
+OBJS += \
+./new-stuff.o \
+./new-test.o 
+
+C_DEPS += \
+./new-stuff.d \
+./new-test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
